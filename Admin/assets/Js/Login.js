9166247883a5cddd70fb6f388c0f0070a80f2f7a
Lucute2001 <lch.ryu2001@gmail.com>
2022-06-20 14:22:09 +0700

    edit path login 2

--- Admin/assets/Js/Login.js
+++ Admin/assets/Js/Login.js
@@ -32,7 +32,8 @@ btnLogin.onclick = function () {
     if (count == 1) {
         alert("Login success");
         // window.location.assign = "Admin.html";
-        window.open('Admin.html');
+        Window.location.assign = "https://lucute2001.github.io/HaoHao_Project/Admin/Admin.html";
+        // window.open('Admin.html');
     }
     else {
         alert("Login failed");
