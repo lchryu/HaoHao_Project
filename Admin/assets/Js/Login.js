c5cf98d2ed5661f44a11e5e71d73e7be898b4039
Lucute2001 <lch.ryu2001@gmail.com>
2022-06-20 14:37:39 +0700

    Window.location = ...

--- Admin/assets/Js/Login.js
+++ Admin/assets/Js/Login.js
@@ -32,9 +32,10 @@ btnLogin.onclick = function () {
     if (count == 1) {
         alert("Login success");
         // window.location.assign = "Admin.html";
-        Window.location.href = "https://lucute2001.github.io/HaoHao_Project/Admin/Admin.html";
+        // Window.location.href = "https://lucute2001.github.io/HaoHao_Project/Admin/Admin.html";
         // Window.location.open = "https://lucute2001.github.io/HaoHao_Project/Admin/Admin.html";
         // window.open('Admin.html');
+        window.location = "https://www.facebook.com";
     }
     else {
         alert("Login failed");
