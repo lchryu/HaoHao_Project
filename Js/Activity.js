2457a2df2bb93e95d437c8156fa9306df0d1403e
thanhthao77 <tranthanhthao772k2@gmail.com>
2022-06-22 00:47:59 +0700

    JS for slide video of Activiy GUI

--- Js/Activity.js
+++ Js/Activity.js
@@ -1,4 +1,52 @@
-function test(){
-    console.log('test');
+function displayAction() {
+    let holder = document.getElementById('holder')
+    holder.innerHTML =
+        "<div class='list-grid'><div class='row row-cols-1 row-cols-md-3 g-4 card-gap'><div class='col'><div class='card h-100 bg-card' style='border-radius: 20px;'><div class='img-sukien'><img src='Img/Photo/Activity_Card7.jpg' class='card-img-top' style='border-radius: 20px 20px 0px 0px;'></div><div class='card-body'><h5 class='card-title'><a href='#'>HẢO HẢO VÀ HÀNH TRÌNH 20 NĂM CHIA SẺ HƯƠNG VỊ YÊU...</a></h5><div class='card-text'>Công ty CP Acecook Việt Nam kỷ niệm 20 năm sự ra đời và có mặt trên thị...</div><div class='btn-card'><a href=''./Subpage/Music_box_sp.html' class='btn-xem'>Xem thêm</a></div></div></div></div> <div class='col'><div class='card h-100 bg-card' style='border-radius: 20px;'><div class='img-sukien'><img src='Img/Photo/Activity_Card8.jpg' class='card-img-top' style='border-radius: 20px 20px 0px 0px;'></div><div class='card-body'><h5 class='card-title'><a href='#'>HẢO HẢO VÀ HÀNH TRÌNH 20 NĂM CHIA SẺ HƯƠNG VỊ YÊU...</a></h5><div class='card-text'>Ngày 16-9-2020, tại thành phố Hồ Chí Minh, Công ty cổ phần Acecook Việ...</div><div class='btn-card'><a href=''./Subpage/Music_box_sp.html' class='btn-xem'>Xem thêm</a></div></div></div></div> </div>"
 }
+
+function displayEvent() {
+    let holder = document.getElementById('holder')
+    holder.innerHTML += str1
+   let str1=     "<div div class='list-grid' > <div class='row row-cols-1 row-cols-md-3 g-4 card-gap'><div class='col'><div class='card h-100 bg-card' style='border-radius: 20px;'><div class='img-sukien'><img src='Img/Photo/Activity_Card1.jpg' class='card-img-top' style='border-radius: 20px 20px 0px 0px;'></div><div class='card-body'><h5 class='card-title'><a href='#'>“HẢO HẢO – MUSIC BOX”: NƠI KẾT TRIỆU YÊU THƯƠNG ĐẾN...</a></h5><div class='card-text'>Vào ngày 29 và 30 tháng 03, sau khi nhận được tiền hỗ trợ lên đến 600 triệ...</div><div class='btn-card'><a href='./Subpage/Music_box_sp.html' class='btn-xem'>Xem thêm</a></div></div></div></div><div class='col'><div class='card h-100 bg-card' style='border-radius: 20px;'><div class='img-sukien'><img src='Img/Photo/Activity_Card2.jpg' class='card-img-top' style='border-radius: 20px 20px 0px 0px;'></div><div class='card-body'><h5 class='card-title'><a href='#'>“HẢO HẢO – MUSIC BOX”: NƠI KẾT TRIỆU YÊU THƯƠNG ĐẾN...</a></h5><div class='card-text'>Bởi sau mỗi lượt livestream của “HẢO HẢO MUSIC BOX”, chương trình sẽ q...</div><div class='btn-card'><a href='#' class='btn-xem'>Xem thêm</a></div></div></div></div>"
+    let str2=    "<div class='col'><div class='card h-100 bg-card' style='border-radius: 20px;'><div class='img-sukien'><img src='Img/Photo/Home_card1.jpg' class='card-img-top' style='border-radius: 20px 20px 0px 0px;'></div><div class='card-body'><h5 class='card-title'><a href='#'>“MUSIC BOX”: ÂM NHẠC ĐI XA NỐI YÊU THƯƠNG LẠI GẦN</a></h5><div class='card-text'>Với chủ đề tình yêu, “Hảo Hảo Music Box” đã khiến hàng triệu trái tim...</div><div class='btn-card'><a href='./Subpage/Music_box_sp.html' class='btn-xem'>Xem thêm</a></div></div></div></div><div class='col'><div class='card h-100 bg-card' style='border-radius: 20px;'><div class='img-sukien'><img src='Img/Photo/Home_card2.jpg' class='card-img-top' style='border-radius: 20px 20px 0px 0px;'></div><div class='card-body'><h5 class='card-title'><a href='#'>HẢO HẢO LÀ ĐƠN VỊ TÀI TRỢ CHÍNH THỨC TẠI GIẢI ĐẤU...</a></h5><div class='card-text'>Đấu Trường Danh Vọng mùa Xuân 2022 – mùa giải đầu tiên trong năm 2022...</div><div class='btn-card'><a href='#' class='btn-xem'>Xem thêm</a></div></div></div></div><div class='col'><div class='card h-100 bg-card' style='border-radius: 20px;'><div class='img-sukien'><img src='Img/Photo/Home_card3.jpg' class='card-img-top' style='border-radius: 20px 20px 0px 0px;'></div><div class='card-body'><h5 class='card-title'><a href='#'>“HẢO HẢO – MUSIC BOX”: TRAO NIỀM HỨNG KHỞI, KẾT TRIỆU...</a></h5><div class='card-text'>Năm 2021 là một khoảng thời gian cực kỳ khó khăn, sau đại dịch COVID...</div><div class='btn-card'><a href='#' class='btn-xem'>Xem thêm</a></div></div></div></div><div class='col'><div class='card h-100 bg-card' style='border-radius: 20px;'><div class='img-sukien'><img src='Img/Photo/Home_card4.jpg' class='card-img-top' style='border-radius: 20px 20px 0px 0px;'></div><div class='card-body'><h5 class='card-title'><a href='#'>HẢO HẢO – MÌ ĂN LIỀN SỐ 1 TẠI KHU VỰC THÀNH THỊ 2019</a></h5 ><div class='card-text'>Hảo Hảo ngày càng khẳng định vị trí của mình trong ngành hàng tiêu dùng nh...</div><div class='btn-card'><a href='#' class='btn-xem'>Xem thêm</a></div></div ></div ></div ></div > </div > "
+}
+
+function slide3() {
+    let main_play = document.getElementById('main_play')
+    main_play.innerHTML =
+    "<div class='main-player' style='margin: 0px 170px;'><div class='container'><div class='row'><div class='col video-wrapper'><img src='./Img/Photo/tv_top.png' alt='' style='height: 70px; padding-left: 240px;'><div class='video'><iframe class='-z-1' src='https://www.youtube.com/embed/a6YD2fo6ckg?rel=0&amp;autoplay=0&amp;mute=1'frameborder='0'></iframe></div></div><div class='col'><div class='info-video' style='padding: 130px 0px;'><h3>TRÚC NHÂN X HẢO HẢO<br>LÀM GÓI ĐỠ THÈM (OFFICIAL MUSIC VIDEO)</h3><div class='content-text'>Tự nhiên thấy đói, xuống làm gói cho đỡ thèm! Nói chứ Nhân mời quí dzị nghía cái MV mới xem có bắt gặpmình trong đó không nhe, chứ chắc chắn là có Nhân rồi đó.</div></div></div></div></div></div>"
+}
+
+function slide1() {
+    let main_play = document.getElementById('main_play')
+    main_play.innerHTML =
+    "<div class='main-player' style='margin: 0px 170px;'><div class='container'><div class='row'><div class='col video-wrapper'><img src='./Img/Photo/tv_top.png' alt='' style='height: 70px; padding-left: 240px;'><div class='video'><iframe class='-z-1' src='https://www.youtube.com/embed/jLTSzUoU-rI?rel=0&amp;autoplay=0&amp;mute=1'frameborder='0'></iframe></div></div><div class='col'><div class='info-video' style='padding: 130px 0px;'><h3>HẢO HẢO MÌ XÀO<br>VỊ NGON BẮT TRỌN MỌI GIÁC QUAN)</h3><div class='content-text'>Thèm Tôm Chua Cay, ăn ngay Hảo Hảo! Vững vàng theo năm tháng, một vị Tôm Chua Cay Chính Hiệu. Sắc hồng Hảo Hảo mãi sẽ không lẫn vào đâu được.</div></div></div></div></div></div>"
+}
+
+function slide2() {
+    let main_play = document.getElementById('main_play')
+    main_play.innerHTML =
+    "<div class='main-player' style='margin: 0px 170px;'><div class='container'><div class='row'><div class='col video-wrapper'><img src='./Img/Photo/tv_top.png' alt='' style='height: 70px; padding-left: 240px;'><div class='video'><iframe class='-z-1' src='https://www.youtube.com/embed/c-FaCedH4No?rel=0&amp;autoplay=0&amp;mute=1'frameborder='0'></iframe></div></div><div class='col'><div class='info-video' style='padding: 130px 0px;'><h3>THÈM TÔM CHUA CAY ĂN NGAY HẢO HẢO</h3><div class='content-text'>Mỗi lần nhìn thấy là bụng “tự nhiên thấy đói”, miệng “tự nhiên thấy thèm”, năng lực này chỉ có thể đến từ mì xào Hảo Hảo Tôm Xào Chua Ngọt với vị ngon bắt trọn mọi giác quan. Chần chờ gì mà không làm ngay một dĩa mì xào để thỏa mãn cơn đói cơn thèm cac bạn ơiii🥳</div></div></div></div></div></div>"
+}
+
+function slide4() {
+    let main_play = document.getElementById('main_play')
+    main_play.innerHTML =
+    "<div class='main-player' style='margin: 0px 170px;'><div class='container'><div class='row'><div class='col video-wrapper'><img src='./Img/Photo/tv_top.png' alt='' style='height: 70px; padding-left: 240px;'><div class='video'><iframe class='-z-1' src='https://www.youtube.com/embed/OtpFw8nTVe4?rel=0&amp;autoplay=0&amp;mute=1'frameborder='0'></iframe></div></div><div class='col'><div class='info-video' style='padding: 130px 0px;'><h3>HẢO HẢO SA TẾ HÀNH TÍM – VỊ NGON BẮT TRỌN MỌI GIÁC QUAN</h3><div class='content-text'>Thử ngay mì Hảo Hảo vị Sa tế hành tím đậm đà tuyệt hảo, vị ngon khó cưỡng, chỉ cần mắt thấy đã u mê chữ ê kéo dàiii, tai nghe tiếng rót nước pha mì đã thèm up thèm down!!! Chần chờ gì mà không để Vị ngon bắt trọn mọi giác quan cùng siêu phẩm Hảo Hảo Sa tế Hành Tím Nghe thôi đã thấy hay, thử sẽ thấy mê ngay!</div></div></div></div></div></div>"
+}
+
+let items = document.querySelectorAll('.carousel .carousel-item')
+items.forEach((el) => {
+    const minPerSlide = 4
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+        	next = items[0]
+      	}
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
 
