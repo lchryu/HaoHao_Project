e23a2e78bcc602d0f5299b6181292aa998401ed8
thanhthao77 <tranthanhthao772k2@gmail.com>
2022-06-22 23:50:32 +0700

    JS for Activity GUI + Promotion GUI

--- Js/Promotion.js
+++ Js/Promotion.js
@@ -1,3 +1,34 @@
-function test(){
-    console.log('test');
+function Page2() {
+    let holder = document.getElementById('holder')
+    document.getElementById("numpage").style.display = "none"
+    holder.innerHTML =
+    "<div class='list-grid' > <div class='row row-cols-1 row-cols-md-3 g-4 card-gap'>\r\
+    <div class='col'><div class='card h-100 bg-card' style='border-radius: 20px;'><div class='img-sukien'><img src='Img/Photo/Card3_promotion.jpg' class='card-img-top' style='border-radius: 20px 20px 0px 0px;'></div><div class='card-body'><h5 class='card-title'><a href='#'>HẢO HẢO LẬP KỶ LỤC SẢN PHẨM MÌ ĂN LIỀN TIÊU THỤ...</a></h5><div class='card-text'>Tổ chức kỷ lục Việt Nam vừa xác lập kỷ lục “Sản phẩm mì gói có số lượng tiê...</div><div class='btn-card'><a href='./Subpage/Ky_Luc_sp.html' class='btn-xem'>Xem thêm</a></div></div></div></div>\r\
+    </div > </div >\r\
+    <div class='number-page'><ul class='justify-content-center' style='display: flex; list-style-type: none;'>\r\
+    <li class='trang-cuoi'><a href=''>Trang đầu</a></li>\r\
+    <li class='trang-1'><a href='' style='color: #444;'>1</a></li>\r\
+    <li class='trang-2 bd-number' onclick='Page2()'><a href='#'style=' background-color: rgba(238, 139, 174);  color: white; padding: 5px 10px; border-radius: 50px;'>2</a></li></ul></div>"
 }
+
+function Nam2021() {
+    let holder = document.getElementById('holder')
+    document.getElementById("numpage").style.display = "none"
+    holder.innerHTML =
+      "<div class='list-grid' style='margin-bottom:40px;'> <div class='row row-cols-1 row-cols-md-3 g-4 card-gap'>\r\
+      <div class='col'><div class='card h-100 bg-card' style='border-radius: 20px;'><div class='img-sukien'><img src='Img/Photo/Card2_promotion.jpg' class='card-img-top' style='border-radius: 20px 20px 0px 0px;'></div><div class='card-body'><h5 class='card-title'><a href='#'> THỂ LỆ CHƯƠNG TRÌNH KHUYẾN MÃI: “HẢO HẢO - ...</a></h5><div class='card-text'>1. Tên chương trình khuyến mại: Hảo Hảo – Music Box 2. Hàng hóa, dịch...</div><div class='btn-card'><a href='' class='btn-xem'>Xem thêm</a></div></div></div></div>\r\
+      <div class='col'><div class='card h-100 bg-card' style='border-radius: 20px;'><div class='img-sukien'><img src='Img/Photo/Card3_promotion.jpg' class='card-img-top' style='border-radius: 20px 20px 0px 0px;'></div><div class='card-body'><h5 class='card-title'><a href='#'>HẢO HẢO LẬP KỶ LỤC SẢN PHẨM MÌ ĂN LIỀN TIÊU THỤ...</a></h5><div class='card-text'>Tổ chức kỷ lục Việt Nam vừa xác lập kỷ lục “Sản phẩm mì gói có số lượng tiê...</div><div class='btn-card'><a href='./Subpage/Ky_Luc_sp.html' class='btn-xem'>Xem thêm</a></div></div></div></div>\r\
+      </div > </div > "
+}
+
+function Nam2022() {
+    let holder = document.getElementById('holder')
+    document.getElementById("numpage").style.display = "none"
+    holder.innerHTML =
+      "<div class='list-grid' style='margin-bottom:40px;'> <div class='row row-cols-1 row-cols-md-3 g-4 card-gap'>\r\
+      <div class='col'><div class='card h-100 bg-card' style='border-radius: 20px;'><div class='img-sukien'><img src='Img/Photo/MB_Card2_sp.jpg' class='card-img-top' style='border-radius: 20px 20px 0px 0px;'></div><div class='card-body'><h5 class='card-title'><a href='#'>DANH SÁCH TRÚNG THƯỞNG VÀ THỂ LỆ – CTKM ĂN MÌ H...</a></h5><div class='card-text'>THÔNG TIN VỀ CHƯƠNG TRÌNH KHUYẾN MẠI DÀNH CHO NGƯ...</div><div class='btn-card'><a href='./Subpage/Ky_Luc_sp.html' class='btn-xem'>Xem thêm</a></div></div></div></div>\r\
+      <div class='col'><div class='card h-100 bg-card' style='border-radius: 20px;'><div class='img-sukien'><img src='Img/Photo/Card1_promotion.jpg' class='card-img-top' style='border-radius: 20px 20px 0px 0px;'></div><div class='card-body'><h5 class='card-title'><a href='#'>THỂ LỆ CHƯƠNG TRÌNH KHUYẾN MÃI: “TẢI APP H...</a></h5><div class='card-text'>Tải App Hảo Hảo phiên bản mới nhất tại Tên chương trình khuyến mại: “Tải Ap...</div><div class='btn-card'><a href='./Subpage/Ky_Luc_sp.html' class='btn-xem'>Xem thêm</a></div></div></div></div>\r\
+      </div > </div > "
+}
+
+    
